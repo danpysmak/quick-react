47bc2dc8c96d2f270040dd9f7e9b4abcd4c8f6e4
Danyil Pysmak <d.pysmak@gmail.com>
2019-11-17 19:04:29 -0600

    adding firebase db config

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
+import firebase from "firebase/app";
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcdnANRmEGpuxYRwbrqqZf0YwVue_jOmg",
+    authDomain: "quick-react-eb43a.firebaseapp.com",
+    databaseURL: "https://quick-react-eb43a.firebaseio.com",
+    projectId: "quick-react-eb43a",
+    storageBucket: "quick-react-eb43a.appspot.com",
+    messagingSenderId: "568267084945",
+    appId: "1:568267084945:web:49e47af270dc517a529b30"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 const days = ['M', 'Tu', 'W', 'Th', 'F'];
@@ -136,7 +151,5 @@ const App = () =>  {
         </Container>
     );
 };
-
-
 
 export default App;
